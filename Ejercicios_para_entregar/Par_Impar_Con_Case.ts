f0c7c714889bdb1d5da818aebc0664a6601bdcf7
Erica Silvana Gramuglia <103612914+eri-gramuglia@users.noreply.github.com>
2022-05-09 18:03:54 -0300

    codigo_Medalla Se agregó el código del ejercicio de medalla

---
+++ Ejercicios_para_entregar/Par_Impar_Con_Case.ts
@@ -0,0 +1,21 @@
+let numero : number = Number (prompt("ingrese el número a verificar si es par o impar"));
+    console.log ("el número ingresado es:", numero);
+
+switch (numero) { 
+  
+  case 1:
+    (numero % 2 === 0)
+    console.log ("el numero es par");
+    break;
+  case 2:
+    (numero % 2 === 1)
+    console.log ("el numero es impar");
+
+  default  (numero === 0)
+      console.log("el numero es igual a cero");
+}
+
+
+
+
+
---
+++ codigosVarios/Medallas.ts
@@ -0,0 +1,17 @@
+let posicionLlegada : number = Number (prompt("ingrese posición de llegada: "));
+console.log ("La posición de llegada esel número ingresado es:", numero);
+
+switch (posicionLlegada) { 
+  case 1:
+    console.log ("Entregar medalla de oro");
+    break;
+  case 2:
+      console.log ("Entregar medalla de plata");
+      break;
+  case 3:
+      console.log ("Entregar medalla de bronce");
+      break;
+  default
+      console.log ("Entregar medalla de participación");
+
+  }
