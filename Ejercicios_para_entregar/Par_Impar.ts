aba3396c94630d7f173f368bdeb7553f940f5a26
Erica Silvana Gramuglia <103612914+eri-gramuglia@users.noreply.github.com>
2022-05-09 13:59:25 -0300

    ejercicioParImpar Se agrega el ejercicio Par/Impar

---
+++ Ejercicios_para_entregar/Par_Impar.ts
@@ -0,0 +1,14 @@
+let numero: number = Number(
+  prompt("ingrese el número a verificar si es par o impar")
+);
+console.log("el número ingresado es:", numero);
+
+if (numero === 0) {
+  console.log("el numero es igual a cero");
+} else {
+  if (numero % 2 == 0) {
+    console.log("el numero es par");
+  } else {
+    console.log("el número es impar");
+  }
+}
