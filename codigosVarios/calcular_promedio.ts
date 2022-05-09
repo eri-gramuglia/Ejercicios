8ee9115f787f31e6370780ec0d5c5fc75123e473
Erica Silvana Gramuglia <103612914+eri-gramuglia@users.noreply.github.com>
2022-05-09 20:45:23 -0300

    calcular_promedio Se agregó el ejercicio Calcular el promedio de 10 notas

---
+++ codigosVarios/calcular_promedio.ts
@@ -0,0 +1,9 @@
+let nota, suma, promedio, contador: number;
+suma = 0;
+for (let contador: number = 1; contador < 11; contador++) {
+  nota = Number(prompt("Ingrese nota " + contador + " : "));
+  suma += nota;
+  //suma = suma + nota;
+}
+promedio = suma / 10;
+console.log("El promedio de las 10 notas es: ", +promedio);
