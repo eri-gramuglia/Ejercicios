88069cf0c4b88c0968cf37e1cd00cd67580e68e0
Erica Silvana Gramuglia <103612914+eri-gramuglia@users.noreply.github.com>
2022-05-09 13:48:21 -0300

    Numero Mayor a 20 Se agregó el código para el ejercicio de Número Mayor a 20

--- codigosVarios/NumMayorA20.ts
+++ codigosVarios/NumMayorA20.ts
@@ -1,7 +1,12 @@
 let numero: number = Number(prompt("ingrese el número a verificar"));
 console.log("el número ingresado es:", numero);
-if (numero > 20) {
-  console.log("el numero es mayor a 20");
+
+if (numero === 0) {
+  console.log("el numero es igual a cero");
 } else {
-  console.log("el número es menor a 20");
+  if (numero > 20) {
+    console.log("el numero es mayor a 20");
+  } else {
+    console.log("el número es menor a 20");
+  }
 }
