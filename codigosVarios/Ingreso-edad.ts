f7f97211db7fea2b3c1c2de3f167ce71073c5319
Erica Silvana Gramuglia <103612914+eri-gramuglia@users.noreply.github.com>
2022-05-14 01:59:18 -0300

    Ejercicio-edad-mayor-de-18-años Se agregó el código del ejercicio Informa si tiene 18 años o no.

---
+++ codigosVarios/Ingreso-edad.ts
@@ -0,0 +1,8 @@
+let edad: number = Number(prompt("Ingrese la edad del usuario:"));
+if (edad >= 18) {
+  console.log("Es mayor de 18 años");
+  console.log("Tiene " + edad);
+} else {
+  console.log("No es mayor de 18 años");
+  console.log("Tiene " + edad);
+}
