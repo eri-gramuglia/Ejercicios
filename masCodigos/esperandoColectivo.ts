699751881d3ff2cd0143642fa763a4a2d11870bd
Erica Silvana Gramuglia <103612914+eri-gramuglia@users.noreply.github.com>
2022-05-14 19:37:23 -0300

    ejercicio-llegada-colectivo Se incorporé el código del ejercicio Simular una espera de un colectivo.

---
+++ masCodigos/esperandoColectivo.ts
@@ -0,0 +1,9 @@
+let llegadaColectivo: string;
+console.log("Esperando el colectivo");
+llegadaColectivo = prompt("¿Llegó el colectivo? (Y/N)");
+
+while (llegadaColectivo == "N") {
+  console.log("Esperando el colectivo");
+  llegadaColectivo = prompt("¿Llegó el colectivo? (Y/N)");
+}
+console.log("Llegó el colectivo");
--- másCódigos/holas.ts
+++
@@ -1,8 +0,0 @@
-let cantHolas: number = Number(
-  prompt("ingresar la cant q se quiere escribir HOLA")
-);
-
-while (cantHolas <= 5) {
-  console.log("Hola");
-  cantHolas = cantHolas + 1;
-}
