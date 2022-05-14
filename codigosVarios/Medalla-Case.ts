ba06c1990f2a77c65b5c8197c8a5420fdcc1b2f2
Erica Silvana Gramuglia <103612914+eri-gramuglia@users.noreply.github.com>
2022-05-14 01:39:05 -0300

    Ejercicio-determinar-medalla-case Se agregó el código de Determinar Medalla con la Estructura CASE.

--- codigosVarios/Medalla-Case.ts
+++ codigosVarios/Medalla-Case.ts
@@ -1,14 +1,16 @@
-let posicionLlegada : number = Number (prompt ("Ingrese la posición de llegada del competidor:"));
-switch (posicionLlegada){
-case1:
-        console.log ("Entregar MEDALLA DE ORO");
-        break;
-  case2:
-        console.log ("Entregar MEDALLA DE PLATA");
-        break;
-  case3:
-        console.log ("Entregar MEDALLA DE BRONCE");
-        break;
-  defaul:
-        console.log ("Entregar MENCIÓN DE PARTICIPACIÓN")
+let posicionLlegada: number = Number(
+  prompt("Ingrese la posición de llegada del competidor:")
+);
+switch (posicionLlegada) {
+  case 1:
+    console.log("Entregar MEDALLA DE ORO");
+    break;
+  case 2:
+    console.log("Entregar MEDALLA DE PLATA");
+    break;
+  case 3:
+    console.log("Entregar MEDALLA DE BRONCE");
+    break;
+  default:
+    console.log("Entregar MENCIÓN DE PARTICIPACIÓN");
 }
---
+++ codigosVarios/Medallas.ts
@@ -0,0 +1,17 @@
+let puestoLlegada : number = Number (prompt("ingrese posición de llegada: "));
+console.log ("La posición de llegada del número ingresado es:", numero);
+
+switch (puestoLlegada) { 
+  case 1:
+    console.log ("Entregar medalla de oro");
+    break;
+  case 2:
+      console.log ("Entregar medalla de plata");
+      break;
+  case 3:
+      console.log ("Entregar medalla de bronce");
+      break;
+  default
+      console.log ("Entregar medalla de participación");
+
+  }
