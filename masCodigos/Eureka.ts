b35aaa742915a2305376ae20556b3556b42e5d8e
Erica Silvana Gramuglia <103612914+eri-gramuglia@users.noreply.github.com>
2022-05-14 22:34:27 -0300

    ejercicio-ingreso-clave Se agregó el código de ingreso de clave

---
+++ masCodigos/Eureka.ts
@@ -0,0 +1,12 @@
+let contraseña: string;
+let cantIntentos: number = 1;
+
+while (cantIntentos <= 3 && contraseña !== "eureka;") {
+  contraseña = prompt("Ingresar la contraseña");
+  cantIntentos++;
+}
+if (contraseña === "eureka") {
+  console.log("Contraseña Aceptada");
+} else {
+  console.log("ERROR DE INGRESO DE CONTRASEÑA");
+}
