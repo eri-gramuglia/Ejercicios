00ff7eaec4b2f56cdfe1d822bb42020767b26e05
Erica Silvana Gramuglia <103612914+eri-gramuglia@users.noreply.github.com>
2022-05-15 06:34:06 -0300

    ejercicio-suma-entre-num-FOR Se agregó el código del ejercicio Suma entre números con estructura de control FOR.

--- Ejercicios_para_entregar/Suma_entre_numeros.ts
+++ Ejercicios_para_entregar/Suma_entre_numeros.ts
@@ -2,14 +2,22 @@ let num1: number = Number(prompt("Ingrese el primer número"));
 let num2: number = Number(prompt("Ingrese el segundo número"));
 let entreNum: number = num1;
 let suma: number = 0;
-//let contador:number= ("");
 
 if (num1 >= num2) {
   console.log("Vuelva a ingresar dos números: ");
 } else {
-  while (entreNum <= num2) {
+  for (entreNum = num1; entreNum <= num2; entreNum++) {
     suma = suma + entreNum;
-    entreNum++;
   }
   console.log("El resultado de la suma es: ", suma);
 }
+
+//if (num1 >= num2) {
+//  console.log("Vuelva a ingresar dos números: ");
+//} else {
+//  while (entreNum <= num2) {
+//    suma = suma + entreNum;
+//    entreNum++;
+//  }
+//  console.log("El resultado de la suma es: ", suma);
+//}
