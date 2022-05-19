8bdbcf7777c8eb6c81734f1338380b546a86aabf
Erica Silvana Gramuglia <103612914+eri-gramuglia@users.noreply.github.com>
2022-05-19 15:52:18 -0300

    ejercicio-Dados Se agregó el ejercicio "Dados".

---
+++ codigosVarios/Dados.ts
@@ -0,0 +1,8 @@
+let numero:number= (prompt("Ingrese cuántas veces va a tirar el dado"));
+let probCara:number=1/6;
+let probFinal:number=1;
+
+for letcontador:number=1; contador<=numero; contador++);{
+  probFinal=probFinal*probCara;
+}
+console.log("La probabilidad es:", probFinal);
