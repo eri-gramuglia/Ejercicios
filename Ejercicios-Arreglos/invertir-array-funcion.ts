d5ec75bf5321afd3387d38b248647e79315e325c
Erica Silvana Gramuglia <103612914+eri-gramuglia@users.noreply.github.com>
2022-05-31 14:10:09 -0300

    funciones-genericas

--- Ejercicios-Arreglos/invertir-array-funcion.ts
+++ Ejercicios-Arreglos/invertir-array-funcion.ts
@@ -3,7 +3,7 @@ let numberArray: number[] = new Array(lenghtArray);
 
 function cargarVector(vector: number[]) {
   let indice: number;
-  for (indice = 0; indice <= lenghtArray; indice++) {
+  for (indice = 0; indice <= vector.length; indice++) {
     vector[indice] = Number(
       prompt(`Ingrese el número que se guardará en la posición: ${indice}:`)
     );
@@ -13,7 +13,7 @@ function cargarVector(vector: number[]) {
 
 function mostrarVector(vector: number[]) {
   let indice: number;
-  for (indice = 0; indice <= lenghtArray; indice++) {
+  for (indice = 0; indice <= vector.length; indice++) {
     //vector[indice]=Number( prompt(`Ingrese el número que se guardará en la posición: ${indice}:`));
     console.log(vector[indice], "");
   }
@@ -23,22 +23,23 @@ function mostrarVectorInvertido(vector: number[]) {
   for (let indice: number = vector.length - 1; (indice) => 0; indice--) {
     console.log(vector[indice]);
   }
+}
 
-
-function invertirVector (vector:number [], cantidad:number){
-  let vectorInvertido:number[] = new Array(cantidad);
-  let indice:number=0;
-  for (let indiceInvertido:number = cantidad - 1; indiceInvertido => 0; indiceInvertido--) {
-    vectorInvertido[indice]=vector[indiceInvertido];
-    indice
-  
-  
-   
-   
-    }
-    
-  
-  
+function invertirVector(vector: number[]) {
+  let cantidad = vector.length;
+  let vectorInvertido: number[] = new Array(cantidad);
+  let indice: number = 0;
+  let indiceInvertido: number;
+  for (
+    indiceInvertido = cantidad - 1;
+    (indiceInvertido) => 0;
+    indiceInvertido--
+  ) {
+    vectorInvertido[indice] = vector[indiceInvertido];
+    indice++;
+  }
+  console.log("El arreglo invertido es: " + vectorInvertido);
+}
 
 cargarVector(numberArray);
 mostrarVector(numberArray);
