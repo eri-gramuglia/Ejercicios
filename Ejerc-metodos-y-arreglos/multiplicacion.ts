56c7bdc46b59b51423669f489404f417785e6361
Erica Silvana Gramuglia <103612914+eri-gramuglia@users.noreply.github.com>
2022-06-02 15:16:03 -0300

    Ejercicio-multiplicacion

--- Ejerc-metodos-y-arreglos/multiplicacion.ts
+++ Ejerc-metodos-y-arreglos/multiplicacion.ts
@@ -2,21 +2,45 @@ let dimensionVector: number = 3;
 let vector1: number[] = new Array(dimensionVector);
 let vector2: number[] = new Array(dimensionVector);
 let vectorResultado: number[] = new Array(dimensionVector);
+let v1: number[] = [1, 2, 3];
+let v2: number[] = [4, 5, 6];
+let v3: number[] = [2, 1, 2];
+let v4: number[] = [1, 2, 1];
+let vResultado: number[] = new Array(3);
 
-function cargarArray(v1: number[], dimension: number) {
+function multiplicarArreglo(
+  v1: number[],
+  v2: number[],
+  v3: number[],
+  v4: number[],
+  v5: number[],
+  dimension: number
+) {
+  for (let indice = 0; indice < dimension; indice++)
+    v5[indice] = v1[indice] * v2[indice] * v3[indice] * v4[indice];
+}
+function cargarArray(v: number[], dimension: number) {
   for (let indice: number = 0; indice < dimension; indice++)
-    v1[indice] = Number(prompt("ingrese los numeros para el vector"));
+    v[indice] = Number(prompt("ingrese los numeros para el vector"));
 }
-function multiplicarArreglo(v1, v2, v3: number[], dimension: number) {
-  for (let indice = 0; indice < dimension; indice++) {
-    v3[dimension] = v1[indice] * v2[indice];
-  }
-  console.log(v3);
+function multiplicarArreglo1(
+  v1: number[],
+  v2: number[],
+  v3: number[],
+  dimension: number
+) {
+  for (let indice = 0; indice < dimension; indice++)
+    v3[indice] = v1[indice] * v2[indice];
 }
-cargarArray(vector1, dimensionVector);
-cargarArray(vector2, dimensionVector);
-multiplicarArreglo(vector1, vector2, vectorResultado, dimensionVector);
 
-//if (vector1.length===vector2.length && vector2.length===vectorResultado.length){
-// multiplicarArreglo(vector1,vector2,vectorResultado,3)
-//}
+cargarArray(vector1, dimensionVector);
+console.log("Datos primer vector:", vector1);
+cargarArray(vector2, dimensionVector);
+console.log("Datos segundo vector:", vector2);
+multiplicarArreglo1(vector1, vector2, vectorResultado, dimensionVector);
+console.log(
+  "La multiplicación de los dos vectores anteriores es:",
+  vectorResultado
+);
+multiplicarArreglo(v1, v2, v3, v4, vResultado, dimensionVector);
+console.log("La multiplicación de los vectores precargados es:", vResultado);
